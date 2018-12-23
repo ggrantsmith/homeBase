0fd60302d3ff903664bba48dde02ce519abd033b
Grant Smith <9725584+ggrantsmith@users.noreply.github.com>
2018-12-23 01:56:23 -0500

    Fixed some deployment issues due to misplaced JS

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mustacheExpress = require('mustache-express')
+var mustacheExpress = require('mustache-express');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,15 +23,3 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 module.exports = app;
-
-
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyD5wmajh23hCvpX3U-ypE9Gozy4sOjvgA8",
-    authDomain: "homebase-51972.firebaseapp.com",
-    databaseURL: "https://homebase-51972.firebaseio.com",
-    projectId: "homebase-51972",
-    storageBucket: "homebase-51972.appspot.com",
-    messagingSenderId: "233750046468"
-  };
-  firebase.initializeApp(config);
