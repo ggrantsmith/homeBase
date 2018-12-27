254c1998d9f69ab3ef10e8135c979e9fc8333d3d
Grant Smith <9725584+ggrantsmith@users.noreply.github.com>
2018-12-27 00:16:02 -0500

    Installed bootstrap. New theme. Removed Semantic UI. FE layout to index.hbs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const exphbs = require('express-handlebars')
+const exphbs = require('express-handlebars');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
